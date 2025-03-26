8258d87554d9b01a3bd6f88826f1eda49592aa6b
robinhelf <100934574+robinhelf@users.noreply.github.com>
2025-03-26 15:06:48 +0800

    Update diy-part1.sh

--- aarch64/diy-part1.sh
+++ aarch64/diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git zerotier https://github.com/mwarning/zerotier-openwrt.git' >>feeds.conf.default
+# echo 'src-git zerotier https://github.com/mwarning/zerotier-openwrt.git' >>feeds.conf.default
 # echo 'src-git haibo https://github.com/haiibo/openwrt-packages.git' >>feeds.conf.default
